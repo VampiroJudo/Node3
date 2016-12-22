3764beee2d372dcbdef6e7bdf86ddb7cf7cc7fab
John <johnconley507@hotmail.com>
2016-12-21 21:00:17 -0500

    correcting code funtion

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = express();
 
 var port = 5000;
+
 app.listen(5000, function(err) {
-	console.log('running server on port');
+	console.log('running server on port ' + port);
 })
