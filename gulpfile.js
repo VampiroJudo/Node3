21bcd12952b6c97786b5c5940648f838d5ed58fd
John <johnconley507@hotmail.com>
2016-12-27 14:57:45 -0500

    updating bowting bower.json file

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,10 @@ gulp.task('style', function() {
 
 gulp.task('inject', function() {
 	var wiredep = require('wiredep').stream;
+	var options = {
+		bowerJson: require('./bower.json'),
+		directory: './public/lib',
+	}
 
 	return gulp.src('./src/views/*.html')
 		.pipe(wiredep(options))
