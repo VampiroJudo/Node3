9d0dfb88b076abec10e55fd3d78ea920770d7200
John <johnconley507@hotmail.com>
2016-12-30 14:13:26 -0500

    creating routes and new books.ejs file

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ var express = require("express");
 var app = express();
 
 var port = 5000;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+	.get(function(req, res){
+		res.send('Hello Books');
+	});
+
+bookRouter.route('/Single')
+	.get(function(req, res){
+		res.send('Hello Single Books');
+	});
+
+
+app.use('/Books', bookRouter)
 
 
 app.get('/', function(req, res) {
