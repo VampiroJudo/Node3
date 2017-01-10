da2b8ac2ab76e95df73fde35cd95523db43f929a
John <johnconley507@hotmail.com>
2017-01-09 23:48:25 -0500

    reverting to pre mssql and starting mongodb

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,6 +1,8 @@
 var express = require('express');
+
 var bookRouter = express.Router();
-var sql = require('mssql');
+
+
 
 var router = function(nav){
 	var books = [
@@ -18,52 +20,20 @@ var router = function(nav){
 			author: 'Alexa Clay/Kyra Maya Phillips'
 		}]
 	bookRouter.route('/')
-	.get(function(req, res){
-		var request = new sql.Request();
-
-		request.query('select * from books',
-					  function(err, recordset){
-					  	res.render('bookListView', {
-							title: 'Books',
-							nav: nav,
-							books: books
-			 });		
+		.get(function (res, req) {
+			res.render('bookListView', {
+				title: 'Books',
+				nav: nav,
+				books: books
+			});
 		});
-	});
 
 
 	bookRouter.route('/:id')
-		.all(function (req, res, next) {
+		.get(function (req, res) {
 			var id = req.params.id;
-			var ps = new sql.PreparedStatement();
-			ps.input('id', sql.Int)
-			ps.prepare('select * from books where id = @id',
-				function (err) {
-					ps.execute({
-							id: req.params.id
-
-						},
-						function (err, recordset) {
-							if (recordset.length === 0) {
-							res.status(404).send('Not Found');
-					
-							} else {
-								req.book = recordset[0];
-								next();
-							}
-						});
-					});
-			})
 			
-			.get(function (req, res){
-				res.render('bookView', {
-					title: 'Books',
-					nav: nav,
-					book: req.book		
-				});
-		
-			});
-	
+		})
 
 	
 	return bookRouter;
