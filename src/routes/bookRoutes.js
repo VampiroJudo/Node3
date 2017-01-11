7051bc94934ff47414e28dac778e70a09b540f2a
John <johnconley507@hotmail.com>
2017-01-11 12:39:30 -0500

    rendering id's

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
-
-
+var objectId = require('mongod').ObjectID;
 
 var router = function(nav){
 	
 	bookRouter.route('/')
 		.get(function (res, req) {
+			var  id = ObjectID(req.params.id);
 			var url = 
 				'mongodb://localhost:27017/libraryApp';
 
 			mongodb.connect(url, function (err, db) {
 			var collection = db.collection('books');
 			
-			collection.find({}).toArray(
+			collection.findOne({_id: id},
 				function(err, results) {
-					res.render('bookListView', {
+					res.render('bookView', {
 						title: 'Books',
 						nav: nav,
 						books: results
@@ -28,14 +28,7 @@ var router = function(nav){
 
 	bookRouter.route('/:id')
 		.get(function (req, res) {
-			var id = req.params.id;
-			res.render('bookView', {
-				title: 'Books',
-				nav: nav,
-				book: books[id]
-			});
 			
-		});
 
 	
 	return bookRouter;
