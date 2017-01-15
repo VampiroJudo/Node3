359833c94ba61ba5d9d6c172ea2e5a61dff61f28
John <johnconley507@hotmail.com>
2017-01-14 19:22:43 -0500

    adding functionality to sign in

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,10 +6,16 @@ var router = function() {
 	authRouter.route('/signUp')
 		.post(function (req,res) {
 			console.log(req.body);
+			req.login(req.body, function() {
+				res.redirect('/auth/profile');
+			});
 		});
+		authRouter.route('/profile')
+			.get(function(req, res){
+				res.json(req.user);
+			});
 		return authRouter;
 
 };
-
 
 module.exports = router;
